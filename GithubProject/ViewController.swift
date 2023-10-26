70736a92ad3955d9acf2ff0b96e6c3da2cd23288
talhade <talhademirkn@gmail.com>
2023-10-26 19:37:17 +0300

    Initial Setup

---
+++ GithubProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubProject
+//
+//  Created by Talha Demirkan on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
