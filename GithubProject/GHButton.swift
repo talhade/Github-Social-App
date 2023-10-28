edfac6c30525cae47205915475349bd53b056490
talhade <talhademirkn@gmail.com>
2023-10-28 12:09:53 +0300

    Added UI Components

---
+++ GithubProject/GHButton.swift
@@ -0,0 +1,35 @@
+//
+//  GHButton.swift
+//  GithubProject
+//
+//  Created by Talha Demirkan on 28.10.2023.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GithubProject/GHTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GHTextField.swift
+//  GithubProject
+//
+//  Created by Talha Demirkan on 28.10.2023.
+//
+
+import UIKit
+
+class GHTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+
+}
