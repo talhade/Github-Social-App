3fe996fc4c869c0935eae6aedfc1a33d89eefc54
talhade <talhademirkn@gmail.com>
2023-11-08 11:19:15 +0300

    Added favoriet screens functionality

--- GithubProject/Screens/GHFavoritesListViewController.swift
+++ GithubProject/Screens/GHFavoritesListViewController.swift
@@ -8,18 +8,72 @@
 import UIKit
 
 class GHFavoritesListViewController: UIViewController {
+    
+    let tableView = UITableView()
+    var favorites: [Follower] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        configureViewController()
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    func configureViewController(){
+        view.backgroundColor = .systemBackground
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureTableView(){
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        PersistenceManager.retrieveFavorites { result in
+        tableView.register(FavoriteTableViewCell.self, forCellReuseIdentifier: FavoriteTableViewCell.reuseID)
+    }
+    
+    func getFavorites(){
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case .success(let favorites):
-                print(favorites)
+                if favorites.isEmpty{
+                    self.showEmptyStateView(with: "No favorites. \nAdd one on the follwer screen ", view: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+                
             case .failure(let error):
-                break
+                self.presentGHAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
+            
         }
     }
+}
+
+extension GHFavoritesListViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteTableViewCell.reuseID) as! FavoriteTableViewCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
 }
